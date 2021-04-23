d5761e06f3f7d2d77b49618a0960399500a3af20
Viktor Säde <viktosd@rackham3.uppmax.uu.se>
2021-04-23 10:35:19 +0200

    Genomeassembly

--- code/2_GenomeAssembly.sh
+++ code/2_GenomeAssembly.sh
@@ -4,7 +4,7 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 8
-#SBATCH -t 36:00:00
+#SBATCH -t 2:00:00
 #SBATCH -J GenomeAssembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Viktor.Sade.7837@student.uu.se
@@ -14,4 +14,4 @@ module load bioinfo-tools
 module load soapdenovo
 
 #
-/sw/apps/bioinfo/SOAPdenovo/2.04-r240/rackham/bin/SOAPdenovo-127mer all -s config_file -K 49 -R -o graph_prefix
+/sw/apps/bioinfo/SOAPdenovo/2.04-r240/rackham/bin/SOAPdenovo-127mer all -s config_file -K 49 -R -o graph_prefix 1>ass.log 2>ass.err
