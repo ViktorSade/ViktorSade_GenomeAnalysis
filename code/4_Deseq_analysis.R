a5dce901588cbce3f87a2f23cd404fed65e7a00c
Viktor Säde <viktor.sade@hotmail.se>
2021-05-23 16:48:58 +0200

    Add files via upload

---
+++ code/4_Deseq_analysis.R
@@ -0,0 +1,31 @@
+
+# Load directory containing the htseq-count files
+directory <- "C:/Users/vikto/Documents/GenomeAnalysis/expression_analysis/HtSeq/"
+
+# Create a list of the file names from the directory
+sampleFiles <- list.files(directory)
+
+# Label each file with the proper condition 
+conditions <- c("Forelimb","Forelimb","Hindlimb","Hindlimb")
+
+#  
+sampleTable <- data.frame(sampleName = sampleFiles, fileName = sampleFiles, condition = conditions)
+sampleTable$condition <- factor(sampleTable$condition)
+
+library(DESeq2)
+
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable, directory = directory, design = ~ condition)
+
+
+dds <- DESeq(ddsHTSeq)
+res <- results(dds)
+
+p_value_subset <- subset(res,pvalue<0.05)
+names <- p_value_subset@rownames
+
+dds_reg <- rlog(dds)
+pheatmap(assay(dds[names]),scale = 'row')
+
+
+vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
+plotPCA(vsd)
